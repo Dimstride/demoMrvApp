951d658b796600b251df79337cc4e0b1b89a2116
Bip <>
2021-06-25 20:08:33 +0200

    Revert "First commit"
    This reverts commit b26af9c2a225562eba69cdc5e192965c740c6281.

--- Marvel SPM/Utils/GoodURL.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-func goodURL(url: String?, format: String?) -> String {
-    if url != nil && format != nil{
-        var urlArray = Array("\(url!).\(format!)")
-        if urlArray[4] != "s" { urlArray.insert("s", at: 4) }
-        return String(urlArray)
-    } else { return "https://www.rosheta.com/upload/e604a3b304e3515f3442a4fbfb7a48eb9435f9ea5eca681a349a2e2ab0a23494.png"}
-}
